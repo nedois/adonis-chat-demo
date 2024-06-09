c25d0df2c24dc90c4809f356bfc0a7e3503f16b4
Natael Bernardo <natael.bernardo@hotmail.com>
2024-06-09 22:48:50 +0200

    handle authentication error

--- app/middleware/socket/socket_auth_middleware.ts
+++ app/middleware/socket/socket_auth_middleware.ts
@@ -2,8 +2,12 @@ import type { SocketMiddleware } from '#services/socket'
 
 /** Use only with long-polling transport, otherwise create a cookie based middleware */
 const SocketAuthMiddleware: SocketMiddleware = async (socket, next) => {
-  await socket.context.auth.authenticateUsing(['api'])
-  next()
+  try {
+    await socket.context.auth.authenticateUsing(['api'])
+    next()
+  } catch (error) {
+    next(new Error(error))
+  }
 }
 
 export default SocketAuthMiddleware
